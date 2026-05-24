7ece9cf8f66d33cbbc1410a9f8bdf36ebb1c6ad9
lumiere4758 <avijaileo@gmail.com>
2026-05-24 09:23:16 +0530

    finish the guide

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+import './style.css';
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop(animate);
+
+renderer.render(scene, camera);
 document.body.appendChild( renderer.domElement );
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const texture = new THREE.TextureLoader().load('zaid.png')
+const texture = new THREE.TextureLoader().load('missy.jpg')
 const material = new THREE.MeshBasicMaterial( { map: texture } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
@@ -17,7 +23,19 @@ const donut_tex = new THREE.MeshBasicMaterial({color: 0xffffff });
 const donut = new THREE.Mesh(donut_geo, donut_tex);
 scene.add(donut);
 
-camera.position.z = 18;
+camera.position.z = 45;
+
+function moveCamera(){
+    const t = document.body.getBoundingClientRect().top;
+
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0000;
+    camera.rotation.y = t * -0.0000;
+
+}
 
 function animate() {
 
@@ -42,3 +60,6 @@ function add_star() {
 Array(200).fill().forEach(add_star);
 
 animate();
+
+document.body.onscroll = moveCamera;
+moveCamera();
